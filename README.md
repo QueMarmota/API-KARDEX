1834d02dd4e91039f904324f3c23bc178c951964
Rafael Alejandro Renteria Gomez <rarg93@hotmail..com>
2018-10-12 10:03:22 -0500

    Autentificacion

--- README.md
+++ README.md
@@ -6,4 +6,27 @@ Paquetes de intalacion
 jwt.sign(payload, secretOrPrivateKey, [options, callback])
         json      llaveprimaria        
 
-https://jwt.io/ para decodificar el token 
+https://jwt.io/ para decodificar el token
+
+AUTORIZACION Y AUTENTIFICACION
+
+Cadena formada por
+                        Tipo de toker
+1) Encabezado--->
+                        Algoritmo de codificacion
+2)Paylod(datos)
+
+3)Firma de verificacion
+
+Node
+
+npm install jsonwebtoken
+
+var jwt require('jsonwebtoken');
+
+jwt.sign(payload,clave,optiones)
+        /login
+Cliente <------>  API
+        /token    genera token
+                  Verificarel token
+
--- servidor.js
+++ servidor.js
@@ -1,22 +1,84 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
-var puerto = process.env.PORT||3000;
+var puerto = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.json());
-app.get('/calificaciones',function(req,res){
-    res.json({
-        mensaje:'Welcome to my API KARDEX'
-    })
+
+//http://localhost:3000/calificaciones?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3VhcmlvIjoiYWx1bW5vIiwibm9tb2JyZSI6IlJhZmFlbCBSZW50ZXJpYSIsImNsYXZlVW5pY2EiOjY2NjY2NiwiaWF0IjoxNTM5MzU1MDg5LCJleHAiOjE1MzkzNTg2ODl9.ilyKZ29ewInwsWxUf2jiaqHWCt9S7d6xDkK9KdUdT40
+//se le pasa por la url el token
+app.get('/calificaciones', function (req, res) {
+    console.log('Token recibido ' + req.query.token);//query recibe los parametros que se mandan atra vez de la url
+    //Una vez recibido el token hay q validar ese token
+    //Autorizacion
+    //esta funcion verify decodifica el token
+    //aqui es donde podemos dar permisos a los usuarios
+    jwt.verify(req.query.token, 'AbrasosNoBalasos',
+        function (error,token) {
+            if (error) {
+                res.status(403).json({ mensaje: 'Autorizacion no valida' })
+            }
+            else {
+                res.json({
+                    mensaje: 'Bienvenido '+token.usuario+' aqui estan las calificaciones...',
+                })
+            }
+        })
+
 })
 
-app.listen(puerto,function(){
-    console.log('Server running in port : '+puerto);
+app.listen(puerto, function () {
+    console.log('Server running in port : ' + puerto);
 });
+/* http://localhost:3000/login
+Body raw json
+{
+	"email":"profesor@uaslp.mx",
+	"password":"abc"
+}
+*/
+app.post('/login', function (req, res) {
+    //Esto simula la base de datos
+    var alumno = {
+        email: 'alumno@uaslp.mx',
+        password: '123'
+    };
+    var profesor={
+        email:'profesor@uaslp.mx',
+        password:'abc'
+    }
+    //Para mandar los datos hay q mandarlos por post pero por el body atravez de un json
+    if (req.body.email == alumno.email && req.body.password == alumno.password) {
+        var token = jwt.sign(
+            {
+                usuario: 'alumno',
+                nomobre: 'Rafael Renteria',
+                claveUnica: 666666
+            },
+            'AbrasosNoBalasos', { expiresIn: '1h' });
+        console.log('Token generated : ' + token);
+        res.json({ ElToken: token ,
+            mensaje: 'Bienvenido Alumno'
+        
+        });
+    }
+    else if (req.body.email == profesor.email && req.body.password == profesor.password) {
+        var token = jwt.sign(
+            {
+                usuario: 'profesor',
+               
+            },
+            'AbrasosNoBalasos', { expiresIn: '1h' });
+        console.log('Token generated : ' + token);
+        res.json({ ElToken: token ,
+            mensaje: 'Bienvenido Profesor'
+        
+        });
+    }
+    else{
+        req.status(401).json({mensaje: 'Credenciales no válidas',
+        ElToken: null
+    })
+    }
 
-app.post('/login',function(req,res){
-    var token = jwt.sign(
-        {usuario:'alumno'}, 'ClaveSuperSecreta',{expiresIn:'60s'} );
-        console.log('Token generated : '+token);
-        res.json({ElToken:token});
 })
